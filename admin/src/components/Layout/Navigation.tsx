5168920e7bf1643e723ca6573ac89bcf71ef82b4
QiusHosens <ffsdp@outlook.com>
2025-03-18 11:47:11 +0800

    修改设置

--- admin/src/components/Layout/Navigation.tsx
+++ admin/src/components/Layout/Navigation.tsx
@@ -1,11 +1,35 @@
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings'; // 引入 MUI 的 Settings 图标
 import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../store';
-import { setThemeMode, setNavPosition } from '../../store/slices/themeSlice';
+import { RootState } from '@/store';
+import { setThemeMode, setNavPosition } from '@/store/slices/themeSlice';
+import { useState } from 'react';
 
 export default function Navigation() {
   const dispatch = useDispatch();
   const { mode, navPosition } = useSelector((state: RootState) => state.theme);
+
+  // 控制 Menu 的状态
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleSettingsClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleThemeToggle = () => {
+    dispatch(setThemeMode(mode === 'light' ? 'dark' : 'light'));
+    handleClose();
+  };
+
+  const handleNavPosition = (position: 'left' | 'top' | 'bottom') => {
+    dispatch(setNavPosition(position));
+    handleClose();
+  };
 
   return (
     <AppBar position="static" sx={{ width: navPosition === 'left' ? 240 : '100%' }}>
@@ -13,12 +37,32 @@ export default function Navigation() {
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           My App
         </Typography>
-        <Button color="inherit" onClick={() => dispatch(setThemeMode(mode === 'light' ? 'dark' : 'light'))}>
-          Toggle {mode === 'light' ? 'Dark' : 'Light'}
-        </Button>
-        <Button color="inherit" onClick={() => dispatch(setNavPosition('left'))}>Left</Button>
-        <Button color="inherit" onClick={() => dispatch(setNavPosition('top'))}>Top</Button>
-        <Button color="inherit" onClick={() => dispatch(setNavPosition('bottom'))}>Bottom</Button>
+        {/* 设置按钮 */}
+        <IconButton color="inherit" onClick={handleSettingsClick}>
+          <SettingsIcon />
+        </IconButton>
+
+        {/* 设置菜单 */}
+        <Menu
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        >
+          <MenuItem onClick={handleThemeToggle}>
+            Switch to {mode === 'light' ? 'Dark' : 'Light'} Theme
+          </MenuItem>
+          <MenuItem onClick={() => handleNavPosition('left')}>
+            Nav Position: Left
+          </MenuItem>
+          <MenuItem onClick={() => handleNavPosition('top')}>
+            Nav Position: Top
+          </MenuItem>
+          <MenuItem onClick={() => handleNavPosition('bottom')}>
+            Nav Position: Bottom
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
