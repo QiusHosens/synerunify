4f101d110a31ecf958c115670d93120657c6507e
QiusHosens <ffsdp@outlook.com>
2025-04-03 21:18:49 +0800

    修改

--- admin/src/layout/MainLayout/NavbarBreadcrumbs.tsx
+++ admin/src/layout/MainLayout/NavbarBreadcrumbs.tsx
@@ -34,40 +34,43 @@ export default function NavbarBreadcrumbs({ routeTree }: NavbarBreadcrumbsProps)
 
   const navigate = useNavigate();
   const location = useLocation();
-  let [activeRoutes, setActiveRoutes] = useState<HomeMenuResponse[]>([]);
+  const [activeRoutes, setActiveRoutes] = useState<HomeMenuResponse[]>([]);
 
   useEffect(() => {
-    let routes: HomeMenuResponse[] = [];
+    const routes: HomeMenuResponse[] = [];
     const findActiveRoute = (items: HomeMenuResponse[]) => {
       items && items.forEach((item) => {
-        if (item.children && item.children.length > 0) {
-          if (isRouteActive(item.path, location.pathname, item.children)) {
-            routes.push(item);
-          }
-          findActiveRoute(item.children);
+        if (isRouteActive(item.path, location.pathname, item.children)) {
+          routes.push(item);
         }
+        findActiveRoute(item.children);
       });
     };
     // debugger
     findActiveRoute(routeTree);
     setActiveRoutes(routes)
     console.log('active routes', routes);
-    
   }, [routeTree, location.pathname]);
+
+  const handleClick = (path: string) => {
+    navigate(path)
+  };
 
   return (
     <StyledBreadcrumbs
       aria-label="breadcrumb"
       separator={<NavigateNextRoundedIcon fontSize="small" />}
     >
-      <Typography variant="body1" >Home</Typography>
-      {activeRoutes.map((item) => {
-        return (
-          <Typography variant="body1" key={item.path} sx={{ color: 'text.primary', fontWeight: 600 }}>
-            {item.name}
-          </Typography>
-        )
-      })}
-    </StyledBreadcrumbs>
+      <Typography variant="body1" sx={{ cursor: 'pointer' }} onClick={() => handleClick('/')}>Home</Typography>
+      {
+        activeRoutes.map((item) => {
+          return (
+            <Typography variant="body1" key={item.path} sx={{ color: 'text.primary', cursor: 'pointer', fontWeight: 400 }} onClick={() => handleClick(item.path)} >
+              {item.name}
+            </Typography>
+          )
+        })
+      }
+    </StyledBreadcrumbs >
   );
 }
