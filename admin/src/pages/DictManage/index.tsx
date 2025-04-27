42848cd162f0c5fd4574589e8f49d272cd2ce706
QiusHosens <ffsdp@outlook.com>
2025-04-27 17:49:07 +0800

    增加字典管理

--- admin/src/pages/DictManage/index.tsx
+++ admin/src/pages/DictManage/index.tsx
@@ -1,59 +1,104 @@
-import { Paper } from '@mui/material';
+import { Box, Button, InputAdornment, Paper, SvgIcon, TextField } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { useEffect } from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
-const paginationModel = { page: 0, pageSize: 5 };
+import { useEffect, useRef, useState } from 'react';
+import { listMenu, MenuQueryCondition, SystemMenuResponse } from '@/api';
+import { GridColDef } from '@mui/x-data-grid';
+import CustomizedDataGridPro from '@/components/CustomizedDataGridPro';
+import SearchIcon from '@/assets/image/svg/search.svg';
+import MenuAdd from './Add';
 
 export default function DictManage() {
   const { t } = useTranslation();
 
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(10);
+  const [pages, setPages] = useState<number>(0);
+  const [total, setTotal] = useState<number>(0);
+  const [keyword, setKeyword] = useState<string>();
+
+  const [records, setRecords] = useState<Array<SystemMenuResponse>>([]);
+
+  const addMenu = useRef();
+
+  const columns: GridColDef[] = [
+    { field: 'name', headerName: t("page.dict.title.name"), width: 200 },
+    { field: 'type', headerName: t("page.dict.title.type"), width: 200 },
+    {
+      field: 'type',
+      headerName: t("page.dict.title.type.remark"),
+      width: 200,
+    },
+    {
+      field: 'sort',
+      headerName: t("page.dict.title.sort"),
+      // type: 'number',
+      width: 200,
+    },
+  ];
+
+  const queryRecords = async () => {
+    let condition: MenuQueryCondition = {
+      page,
+      size,
+      keyword
+    };
+    let result = await listMenu();
+    console.log('menu list', result);
+    result.forEach(menu => {
+      let path = menu.path;
+      let hierarchy = [];
+      if (path) {
+        let paths = path.split('/');
+        for (let p of paths) {
+          if (p) {
+            hierarchy.push(p);
+          }
+        }
+      }
+      menu.hierarchy = hierarchy;
+    })
+    setRecords(result);
+  }
+
+  const getTreeDataPath = (row: any) => row.hierarchy;
+
+  const handleClickOpen = () => {
+    (addMenu.current as any).show();
+  }
+
   useEffect(() => {
-    
+    queryRecords();
   }, []);
 
   return (
-      <Paper sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{ pagination: { paginationModel } }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-        sx={{ border: 0 }}
-      />
-    </Paper>
+    <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <Box sx={{ mb: 2, display: 'flex', justifyContent: 'space-between' }}>
+        <TextField
+          label={t('global.condition.keyword')}
+          sx={{ m: 0, width: '200px' }}
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SvgIcon component={SearchIcon} inheritViewBox />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+        <Button variant="contained" onClick={handleClickOpen}>
+          {t('global.operate.add')}
+        </Button>
+      </Box>
+      <Paper sx={{ flex: 1, width: '100%' }}>
+        <CustomizedDataGridPro
+          columns={columns}
+          initialRows={records}
+          getTreeDataPath={getTreeDataPath}
+          hideFooter={true}
+        />
+      </Paper>
+      <MenuAdd ref={addMenu} />
+    </Box>
   );
 }
