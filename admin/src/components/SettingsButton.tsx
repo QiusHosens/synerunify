0cd62375ad28d586376b6a48540b85d8fb24edff
QiusHosens <ffsdp@outlook.com>
2025-05-24 22:31:21 +0800

    修改

--- admin/src/components/SettingsButton.tsx
+++ admin/src/components/SettingsButton.tsx
@@ -1,9 +1,16 @@
-import { IconButton, Menu, MenuItem, Divider, useTheme, Box } from '@mui/material';
+import { IconButton, Menu, MenuItem, Divider, useTheme, Box, SvgIcon, Stack } from '@mui/material';
 import { styled, SxProps, Theme, useColorScheme } from "@mui/material/styles";
 import SettingsIcon from '@mui/icons-material/Settings';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useThemeStore } from '@/store';
 import { useTranslation } from 'react-i18next';
+import DarkIcon from '@/assets/image/svg/dark.svg';
+import LigntIcon from '@/assets/image/svg/light.svg';
+import LayoutLeftIcon from '@/assets/image/svg/layout_left.svg';
+import LayoutTopIcon from '@/assets/image/svg/layout_top.svg';
+import LayoutLeftSmallIcon from '@/assets/image/svg/layout_left_small.svg';
+import ThemeColorIcon from '@/assets/image/svg/theme_color.svg';
+import FontIcon from '@/assets/image/svg/font.svg';
 
 interface SettingsButtonProps {
   sx?: SxProps<Theme>;
@@ -30,6 +37,10 @@ export default function SettingsButton({ sx }: SettingsButtonProps) {
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+
+  // useEffect(() => {
+  //     handleSettingsClick();
+  //   }, []);
 
   const handleSettingsClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -106,6 +117,18 @@ export default function SettingsButton({ sx }: SettingsButtonProps) {
         anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
         transformOrigin={{ vertical: 'top', horizontal: 'right' }}
       >
+        <Stack direction="row" sx={{ gap: 3, '& .MuiBox-root': { p: 4 } }}>
+          <Box>
+            <SvgIcon inheritViewBox>
+              <DarkIcon />
+            </SvgIcon>
+          </Box>
+          <Box>
+            <SvgIcon inheritViewBox sx={{ color: 'primary.main' }}>
+              <LigntIcon color='primary.main' />
+            </SvgIcon>
+          </Box>
+        </Stack>
         {/* 主题模式 */}
         <MenuItem onClick={handleThemeToggle}>
           {t('switch_theme', { mode: mode === 'light' ? 'Dark' : 'Light' })}
