b679eebc2ef7991155b86e2819124dfcd27ce6b0
QiusHosens <ffsdp@outlook.com>
2025-07-05 07:50:14 +0800

    修改脚本

--- server/module/module-third/module-captcha/captcha-service/transform.sh
+++ server/module/module-third/module-captcha/captcha-service/transform.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker save -o captcha-service.tar synerunify/captcha-service
-ctr -n k8s.io images import captcha-service.tar
-rm captcha-service.tar
+docker save -o service-captcha.tar synerunify/service-captcha
+ctr -n k8s.io images import service-captcha.tar
+rm service-captcha.tar
