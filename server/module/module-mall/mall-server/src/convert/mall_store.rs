e87d8c106b44fa20f0a7ecf95b71035fc6836eed
QiusHosens <ffsdp@outlook.com>
2025-11-10 15:11:34 +0800

    修改接口

--- server/module/module-mall/mall-server/src/convert/mall_store.rs
+++ server/module/module-mall/mall-server/src/convert/mall_store.rs
@@ -5,7 +5,6 @@ use mall_model::response::mall_store::MallStoreResponse;
 
 pub fn create_request_to_model(request: &CreateMallStoreRequest) -> MallStoreActiveModel {
     MallStoreActiveModel {
-        number: Set(request.number.clone()),
         name: Set(request.name.clone()),
         short_name: request.short_name.as_ref().map_or(NotSet, |short_name| Set(Some(short_name.clone()))),
         file_id: Set(request.file_id.clone()),
@@ -14,26 +13,12 @@ pub fn create_request_to_model(request: &CreateMallStoreRequest) -> MallStoreAct
         slogan: request.slogan.as_ref().map_or(NotSet, |slogan| Set(Some(slogan.clone()))),
         description: request.description.as_ref().map_or(NotSet, |description| Set(Some(description.clone()))),
         tags: request.tags.as_ref().map_or(NotSet, |tags| Set(Some(tags.clone()))),
-        status: Set(request.status.clone()),
-        audit_remark: request.audit_remark.as_ref().map_or(NotSet, |audit_remark| Set(Some(audit_remark.clone()))),
-        audit_time: request.audit_time.as_ref().map_or(NotSet, |audit_time| Set(Some(audit_time.clone()))),
-        score_desc: request.score_desc.as_ref().map_or(NotSet, |score_desc| Set(Some(score_desc.clone()))),
-        score_service: request.score_service.as_ref().map_or(NotSet, |score_service| Set(Some(score_service.clone()))),
-        score_delivery: request.score_delivery.as_ref().map_or(NotSet, |score_delivery| Set(Some(score_delivery.clone()))),
-        total_sales_amount: request.total_sales_amount.as_ref().map_or(NotSet, |total_sales_amount| Set(Some(total_sales_amount.clone()))),
-        total_order_count: request.total_order_count.as_ref().map_or(NotSet, |total_order_count| Set(Some(total_order_count.clone()))),
-        total_goods_count: request.total_goods_count.as_ref().map_or(NotSet, |total_goods_count| Set(Some(total_goods_count.clone()))),
-        total_fans_count: request.total_fans_count.as_ref().map_or(NotSet, |total_fans_count| Set(Some(total_fans_count.clone()))),
-        is_recommend: request.is_recommend.as_ref().map_or(NotSet, |is_recommend| Set(Some(is_recommend.clone()))),
         ..Default::default()
     }
 }
 
 pub fn update_request_to_model(request: &UpdateMallStoreRequest, existing: MallStore) -> MallStoreActiveModel {
     let mut active_model: MallStoreActiveModel = existing.into();
-    if let Some(number) = &request.number { 
-        active_model.number = Set(number.clone());
-    }
     if let Some(name) = &request.name { 
         active_model.name = Set(name.clone());
     }
@@ -57,39 +42,6 @@ pub fn update_request_to_model(request: &UpdateMallStoreRequest, existing: MallS
     }
     if let Some(tags) = &request.tags { 
         active_model.tags = Set(Some(tags.clone()));
-    }
-    if let Some(status) = &request.status { 
-        active_model.status = Set(status.clone());
-    }
-    if let Some(audit_remark) = &request.audit_remark { 
-        active_model.audit_remark = Set(Some(audit_remark.clone()));
-    }
-    if let Some(audit_time) = &request.audit_time { 
-        active_model.audit_time = Set(Some(audit_time.clone()));
-    }
-    if let Some(score_desc) = &request.score_desc { 
-        active_model.score_desc = Set(Some(score_desc.clone()));
-    }
-    if let Some(score_service) = &request.score_service { 
-        active_model.score_service = Set(Some(score_service.clone()));
-    }
-    if let Some(score_delivery) = &request.score_delivery { 
-        active_model.score_delivery = Set(Some(score_delivery.clone()));
-    }
-    if let Some(total_sales_amount) = &request.total_sales_amount { 
-        active_model.total_sales_amount = Set(Some(total_sales_amount.clone()));
-    }
-    if let Some(total_order_count) = &request.total_order_count { 
-        active_model.total_order_count = Set(Some(total_order_count.clone()));
-    }
-    if let Some(total_goods_count) = &request.total_goods_count { 
-        active_model.total_goods_count = Set(Some(total_goods_count.clone()));
-    }
-    if let Some(total_fans_count) = &request.total_fans_count { 
-        active_model.total_fans_count = Set(Some(total_fans_count.clone()));
-    }
-    if let Some(is_recommend) = &request.is_recommend { 
-        active_model.is_recommend = Set(Some(is_recommend.clone()));
     }
     active_model
 }
