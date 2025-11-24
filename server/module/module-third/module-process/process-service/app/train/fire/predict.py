b8daeef4261076a2775b8c75c076221fe20d7db0
QiusHosens <ffsdp@outlook.com>
2025-11-24 21:46:04 +0800

    修改

--- server/module/module-third/module-process/process-service/app/train/fire/predict.py
+++ server/module/module-third/module-process/process-service/app/train/fire/predict.py
@@ -5,7 +5,7 @@ model = YOLO('runs/detect/yolo11_custom_training/weights/best.pt')
 
 # 对一张从未见过的图片进行预测
 # save=True 会将结果图片保存在 runs/detect/predict 文件夹中
-results = model.predict(source='path/to/test/image.jpg', save=True, conf=0.5)
+results = model.predict(source='./samples/fire.jpg', save=True, conf=0.5)
 
 # 如果你想直接处理结果
 for result in results:
